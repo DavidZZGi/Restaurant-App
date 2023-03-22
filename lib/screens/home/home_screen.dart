63c6b29f4f542e2c2b06d56c3e9ada14fe1a0831
Ezequiel Chao <ezesoftapple@gmail.com>
2023-03-22 02:29:14 -0400

    added a indexedstack

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,13 @@
 // ignore: file_names
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/model/models.dart';
+import 'package:restaurant_app/screens/cart/cart_screen.dart';
 
 import '../../widgets/widgets.dart';
 
-class HomeAppScreen extends StatelessWidget {
+class HomeAppScreen extends StatefulWidget {
   static const String routeName = '/';
   const HomeAppScreen({Key? key}) : super(key: key);
   static Route route() {
@@ -17,48 +19,98 @@ class HomeAppScreen extends StatelessWidget {
   }
 
   @override
+  State<HomeAppScreen> createState() => _HomeAppScreenState();
+}
+
+class _HomeAppScreenState extends State<HomeAppScreen> {
+  var _courentTab = 0;
+  final List<Widget> tabs = [
+    const HomeContent(),
+    const CartScreen(),
+    const Center(
+      child: Text('Cuenta'),
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(
-        title: 'BettyBoom',
-      ),
-      bottomNavigationBar: const BottomNavBar(),
-      // ignore: avoid_unnecessary_containers
-      body: Column(
-        children: [
-          CarouselSlider(
-            options: CarouselOptions(
-              aspectRatio: 1.5,
-              viewportFraction: 0.9,
-              enlargeCenterPage: true,
-              enlargeStrategy: CenterPageEnlargeStrategy.height,
-              enableInfiniteScroll: true,
-              autoPlay: true,
-            ),
-            items: Category.listCategories
-                .map((e) => CardCarousel(category: e))
-                .toList(),
+      appBar: _courentTab == 0
+          ? const CustomAppBar(
+              title: 'BettyBoom',
+            )
+          : null,
+      // bottomNavigationBar: const BottomNavBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _courentTab,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-          const SectionTitle(
-            title: 'Popular',
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carrito',
           ),
-          Expanded(
-            child: ProductCarusel(
-                products: Product.products
-                    .where((element) => element.isPopular)
-                    .toList()),
-          ),
-          const SectionTitle(
-            title: 'Recommended',
-          ),
-          Expanded(
-            child: ProductCarusel(
-                products: Product.products
-                    .where((element) => element.isRecommended)
-                    .toList()),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person_alt_circle),
+            label: 'Cuenta',
           ),
         ],
+        onTap: (index) {
+          setState(() {
+            _courentTab = index;
+          });
+        },
       ),
+      // ignore: avoid_unnecessary_containers
+      body: IndexedStack(
+        index: _courentTab,
+        children: tabs,
+      ),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            aspectRatio: 1.5,
+            viewportFraction: 0.9,
+            enlargeCenterPage: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.height,
+            enableInfiniteScroll: true,
+            autoPlay: true,
+          ),
+          items: Category.listCategories
+              .map((e) => CardCarousel(category: e))
+              .toList(),
+        ),
+        const SectionTitle(
+          title: 'Popular',
+        ),
+        Expanded(
+          child: ProductCarusel(
+              products: Product.products
+                  .where((element) => element.isPopular)
+                  .toList()),
+        ),
+        const SectionTitle(
+          title: 'Recommended',
+        ),
+        Expanded(
+          child: ProductCarusel(
+              products: Product.products
+                  .where((element) => element.isRecommended)
+                  .toList()),
+        ),
+      ],
     );
   }
 }
